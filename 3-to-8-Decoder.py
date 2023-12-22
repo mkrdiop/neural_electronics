4013311ef7fe663907b6da31421471a54d457670
Makhtar Diop <67843975+mkrdiop@users.noreply.github.com>
2023-12-22 15:11:00 +0000

    Add files via upload

---
+++ 3-to-8-Decoder.py
@@ -0,0 +1,72 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+# Function to simulate the behavior of a 3-to-8 decoder
+def simulate_3_to_8_decoder(input_data):
+    outputs = np.zeros((len(input_data), 8))
+
+    for i in range(len(outputs)):
+        A, B, C = input_data[i]
+        outputs[i] = [
+            int(not A and not B and not C),
+            int(not A and not B and C),
+            int(not A and B and not C),
+            int(not A and B and C),
+            int(A and not B and not C),
+            int(A and not B and C),
+            int(A and B and not C),
+            int(A and B and C)
+        ]
+
+    return outputs
+
+# Function to generate a dataset for the 3-to-8 decoder
+def generate_3_to_8_decoder_dataset(num_samples):
+    input_data = np.random.randint(0, 2, size=(num_samples, 3))  # Random binary inputs (A, B, C)
+    outputs = simulate_3_to_8_decoder(input_data)  # Simulate the 3-to-8 decoder outputs
+
+    return input_data, outputs
+
+# Function to create a neural network model for 3-to-8 decoder emulation
+def create_3_to_8_decoder_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(16, activation='relu', input_shape=(3,)),  # Hidden layer with 16 neurons
+        tf.keras.layers.Dense(8, activation='softmax')  # Output layer with 8 neurons for multi-class classification
+    ])
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+# Generate a dataset for the 3-to-8 decoder
+num_samples = 10000  # Increase the dataset size
+input_data, outputs = generate_3_to_8_decoder_dataset(num_samples)
+
+# One-hot encode the output labels
+outputs_onehot = tf.keras.utils.to_categorical(outputs.argmax(axis=1), num_classes=8)
+
+# Split the dataset into training and testing sets
+train_input, test_input, train_outputs, test_outputs = train_test_split(
+    input_data, outputs_onehot, test_size=0.2, random_state=42
+)
+
+# Create and compile the model
+decoder_3_to_8_model = create_3_to_8_decoder_model()
+
+# Train the model
+decoder_3_to_8_model.fit(
+    train_input,
+    train_outputs,
+    epochs=100,  # Increase the number of epochs
+    verbose=1,
+    validation_split=0.2
+)
+
+# Evaluate the model on the test set
+test_loss, test_accuracy = decoder_3_to_8_model.evaluate(
+    test_input,
+    test_outputs,
+    verbose=0
+)
+
+print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
